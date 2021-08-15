595928e70f992245fc3cac44647655c17514cdc9
ElzaanVan <elzaanvanl@gmail.com>
2021-08-15 13:05:14 +0200

    Linked-lists + Refactored code some more
    Implemented LinkedList, and made class Sorter an abstract class

--- build/index.js
+++ build/index.js
@@ -1,12 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
+var NumbersCollection_1 = require("./NumbersCollection");
 var CharactersCollection_1 = require("./CharactersCollection");
-// const numbersCollection = new NumbersCollection([50, 10, 9, 5, 0, -5]);
+var LinkedList_1 = require("./LinkedList");
+var numbersCollection = new NumbersCollection_1.NumbersCollection([50, 10, 9, 5, 0, -8]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection("mewDentiLoandieElzaanNakha");
+charactersCollection.sort();
+console.log(charactersCollection.data);
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(50);
+linkedList.add(-12);
+linkedList.add(3);
+linkedList.add(5);
+linkedList.sort();
+linkedList.print();
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
-var charactersCollection = new CharactersCollection_1.CharactersCollection("helloW");
-var sorter = new Sorter_1.Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection("helloW");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data)
+// const linkedList = new LinkedList();
+// linkedList.add(50)
+// linkedList.add(-12)
+// linkedList.add(3)
+// linkedList.add(5)
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+// linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,45 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([50, 10, 9, 5, 0, -5]);
+const numbersCollection = new NumbersCollection([50, 10, 9, 5, 0, -8]);
+numbersCollection.sort()
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("mewDentiLoandieElzaanNakha");
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+
+const linkedList = new LinkedList();
+linkedList.add(50)
+linkedList.add(-12)
+linkedList.add(3)
+linkedList.add(5)
+
+linkedList.sort();
+linkedList.print();
+
+
+
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection("helloW");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data)
+// const charactersCollection = new CharactersCollection("helloW");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data)
+
+// const linkedList = new LinkedList();
+// linkedList.add(50)
+// linkedList.add(-12)
+// linkedList.add(3)
+// linkedList.add(5)
+
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+// linkedList.print();
+
+
