90ebf9d7e9912e23db588f3c5d84c89ad9885a73
ElzaanVan <elzaanvanl@gmail.com>
2021-08-15 11:34:28 +0200

    Interface
    Created an Interface to make sort more generic for all types of data

--- build/index.js
+++ build/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
-var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 9, 5, 0, -5]);
+var numbersCollection = new NumbersCollection_1.NumbersCollection([50, 10, 9, 5, 0, -5]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 
-const numbersCollection = new NumbersCollection([10, 9, 5, 0, -5]);
+const numbersCollection = new NumbersCollection([50, 10, 9, 5, 0, -5]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
